169a018a2e56b4da6d7efe45c96c3340191ddf14
stevenwonghk <steven.wong.hk@gmail.com>
2022-12-30 22:06:17 +0800

    Customized 404 page

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import createError from 'http-errors';
 import express from 'express';
 import Twig from 'twig';
 import path from 'path';
@@ -17,4 +18,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use((req, res, next) => {
+    next(createError(404));
+});
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 export default app;
