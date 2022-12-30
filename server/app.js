78256245ad9555501a06c9b33f19f0511e525aca
stevenwonghk <steven.wong.hk@gmail.com>
2022-12-30 17:43:45 +0800

    Add Handlebars template engine

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { create } from 'express-handlebars';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 var app = express();
+var hbs = create({ /* config */ });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './server/views');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,4 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/test', (req, res) => {
+    res.render('home', {body: 'abcd'});
+});
 export default app;
